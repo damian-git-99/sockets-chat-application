4af50f19849809275e0a3695172ea22428175731
damian <damiangalvan66@gmail.com>
2023-05-25 10:13:16 -0600

    chore(package.json): downgrade express-validator to version 6.15.0 feat(expressValidator.ts): add middleware to validate fields using express-validator feat(auth.ts): add validation to register and login routes using express-validator and expressValidator middleware

--- backend-node/src/routes/auth.ts
+++ backend-node/src/routes/auth.ts
@@ -1,7 +1,44 @@
 import { Router } from 'express'
 import { login, register, renewToken } from '../controllers/auth'
+import { body } from 'express-validator'
+import { validateFields } from '../middlewares/expressValidator'
 export const router = Router()
+
 // @route api/v1/auth
-router.post('/register', register)
-router.post('/login', login)
+router.post(
+  '/register',
+  [
+    body('username').notEmpty().withMessage('Username is required'),
+    body('email')
+      .isEmail()
+      .withMessage('Email is invalid')
+      .notEmpty()
+      .withMessage('Email is required'),
+    body('password')
+      .notEmpty()
+      .withMessage('Password is required')
+      .isLength({ min: 6 })
+      .withMessage('Password must be at least 6 characters'),
+    validateFields
+  ],
+  register
+)
+router.post(
+  '/login',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email is invalid')
+      .notEmpty()
+      .withMessage('Email is required'),
+    body('password')
+      .notEmpty()
+      .withMessage('Password is required')
+      .isLength({ min: 6 })
+      .withMessage('Password must be at least 6 characters'),
+    validateFields
+  ],
+  login
+)
+
 router.get('/renew-token', renewToken)
