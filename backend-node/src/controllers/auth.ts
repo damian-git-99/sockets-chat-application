3c7edfa48aceae053b42b4413ed7acd14ecbb359
damian <damiangalvan66@gmail.com>
2023-05-25 10:56:26 -0600

    feat(auth.ts): add user registration functionality feat(passwordUtils.ts): add password encryption and comparison functions chore(package.json): add @types/bcrypt as a dev dependency

--- backend-node/src/controllers/auth.ts
+++ backend-node/src/controllers/auth.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from 'express'
+import { UserModel } from '../models/User'
+import { encryptPassword } from '../utils/passwordUtils'
 
 // @route POST api/v1/auth/register
-export const register = (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response) => {
+  const { username, email, password } = req.body
+
+  const exists = await UserModel.findOne({ email })
+
+  if (exists) {
+    throw new Error('Email already exists')
+  }
+
+  const hashedPassword = encryptPassword(password)
+
+  await UserModel.create({
+    username,
+    email,
+    password: hashedPassword
+  })
+
   res.json({
     ok: true
   })
