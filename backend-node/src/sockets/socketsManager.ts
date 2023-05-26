25ce72099708ad830ca28802a0074600ee572600
damian <damiangalvan66@gmail.com>
2023-05-26 08:56:04 -0600

    feat(socketsManager.ts): add TODOs for JWT validation, user retrieval, connected users emission, socket joining, personal messages handling, and disconnect handling

--- backend-node/src/sockets/socketsManager.ts
+++ backend-node/src/sockets/socketsManager.ts
@@ -4,4 +4,12 @@ import { DefaultEventsMap } from 'socket.io/dist/typed-events'
 interface IO
   extends Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any> {}
 
-export function socketsManager(io: IO) {}
+export function socketsManager(io: IO) {
+  // TODO: validate JWT
+  // if token is not valid disconnect
+  // TODO: get user from token
+  // TODO: emit all connected users
+  // TODO: socket join
+  // TODO: listening to clients -> personal messages
+  // TODO: handle disconnects
+}
