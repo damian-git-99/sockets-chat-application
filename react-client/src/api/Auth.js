c0ee50f8fbf752db0a148b8bb572e055631423e3
damian <damiangalvan66@gmail.com>
2023-05-31 12:19:22 -0600

    feat(Auth.js): add functions for login, register and renew token requests feat(getErrors.js): add function to extract error messages from axios error object

---
+++ react-client/src/api/Auth.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { getErrorMessage } from './getErrors'
+
+const URL = 'http://localhost:8080/api/v1/auth'
+
+const config = {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+export async function loginRequest (email, password) {
+  try {
+    const { data } = await axios.post(`${URL}/login`, { email, password }, config)
+    return data
+  } catch (error) {
+    console.log(error)
+    const message = getErrorMessage(error)
+    throw new Error(message)
+  }
+}
+
+export async function registerRequest (username, email, password) {
+  try {
+    const { data } = await axios.post(`${URL}/register`, { username, email, password }, config)
+    return data
+  } catch (error) {
+    const message = getErrorMessage(error)
+    throw new Error(message)
+  }
+}
+
+export async function renewTokenRequest (token) {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+    const { data } = await axios.get(`${URL}/renew-token`, null, config)
+    return data
+  } catch (error) {
+    const message = getErrorMessage(error)
+    throw new Error(message)
+  }
+}
