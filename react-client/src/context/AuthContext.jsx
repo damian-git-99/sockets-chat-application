98bf43b50dcf4f136ff1e0cbe7a6a92650e8ad2a
damian <damiangalvan66@gmail.com>
2023-05-31 12:19:43 -0600

    feat(AuthContext.jsx): add error state and handle errors in login, register and verifyToken functions

--- react-client/src/context/AuthContext.jsx
+++ react-client/src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { useCallback, useState } from 'react'
+import { loginRequest, registerRequest, renewTokenRequest } from '../api/Auth'
 
 export const AuthContext = React.createContext({})
 
@@ -13,25 +14,69 @@ const initialState = {
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState(initialState)
+  const [error, setError] = useState(null)
 
-  const login = (email, password) => {
-
+  const login = async (email, password) => {
+    try {
+      setError(null)
+      const data = await loginRequest(email, password)
+      const { user, token } = data
+      setAuth({
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        logged: true,
+        checking: false
+      })
+      localStorage.setItem('token', token)
+    } catch (error) {
+      setError(error.message)
+    }
   }
 
-  const register = (username, email, password) => {
-
+  const register = async (username, email, password) => {
+    try {
+      setError(null)
+      const data = await registerRequest(username, email, password)
+      const { user, token } = data
+      setAuth({
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        logged: true,
+        checking: false
+      })
+      localStorage.setItem('token', token)
+    } catch (error) {
+      setError(error.message)
+    }
   }
 
-  const verifyToken = useCallback(() => {
-
+  const verifyToken = useCallback(async () => {
+    try {
+      setError(null)
+      const token = localStorage.getItem('token')
+      const data = await renewTokenRequest(token)
+      const { user, token: newToken } = data
+      setAuth({
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        logged: true,
+        checking: false
+      })
+      localStorage.setItem('token', newToken)
+    } catch (error) {
+      setError(error.message)
+    }
   }, [])
 
   const logout = () => {
-
+    localStorage.removeItem('token')
   }
 
   return (
-    <AuthContext.Provider value={{ auth, login, register, verifyToken, logout }}>
+    <AuthContext.Provider value={{ auth, login, register, verifyToken, logout, error }}>
       {children}
     </AuthContext.Provider>
   )
