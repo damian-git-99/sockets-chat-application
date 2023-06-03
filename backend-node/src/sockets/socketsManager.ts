578c9624a301b09ab90ae66948aeb6b560985b06
damian <damiangalvan66@gmail.com>
2023-06-03 10:09:03 -0600

    feat(socketsManager.ts): add socket connection and disconnection logs

--- backend-node/src/sockets/socketsManager.ts
+++ backend-node/src/sockets/socketsManager.ts
@@ -5,6 +5,13 @@ interface IO
   extends Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any> {}
 
 export function socketsManager(io: IO) {
+  io.on('connection', (client) => {
+    console.log('Client connected')
+
+    client.on('disconnect', (client) => {
+      console.log('Client disconnected')
+    })
+  })
   // TODO: validate JWT
   // if token is not valid disconnect
   // TODO: get user from token
