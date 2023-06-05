4a99fc2c9f59b38be0f98774614e7971e6a04ba0
damian <damiangalvan66@gmail.com>
2023-06-05 10:50:33 -0600

    feat(SidebarChatItem.jsx): add onClick function to activate chat with user feat(ChatPage.jsx): show Messages component only if there is an active chat in chatState

--- react-client/src/components/SidebarChatItem.jsx
+++ react-client/src/components/SidebarChatItem.jsx
@@ -1,7 +1,16 @@
+import { useContext } from 'react'
+import { ChatContext } from '../context/chat/ChatContext'
+
 /* eslint-disable react/prop-types */
 export const SidebarChatItem = ({ user = {} }) => {
+  const { chatState, activateChat } = useContext(ChatContext)
+
+  const onClick = () => {
+    activateChat(user?._id)
+  }
+
   return (
-    <div className="chat_list">
+    <div className={`chat_list ${user?._id === chatState?.activeChat && 'active_chat'}`} onClick={onClick}>
       <div className="chat_people">
         <div className="chat_img">
           <img
--- react-client/src/pages/ChatPage.jsx
+++ react-client/src/pages/ChatPage.jsx
@@ -1,15 +1,19 @@
+import { useContext } from 'react'
 import { ChatSelect } from '../components/ChatSelect'
 import { InboxPeople } from '../components/InboxPeople'
 import { Messages } from '../components/Messages'
 import '../css/chat.css'
+import { ChatContext } from '../context/chat/ChatContext'
 
 export const ChatPage = () => {
+  const { chatState } = useContext(ChatContext)
+
   return (
     <div className="messaging">
       <div className="inbox_msg">
         <InboxPeople />
 
-        {!true ? <Messages /> : <ChatSelect />}
+        {chatState?.activeChat ? <Messages /> : <ChatSelect />}
       </div>
     </div>
   )
